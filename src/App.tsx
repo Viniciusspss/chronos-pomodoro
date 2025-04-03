af14251a2be3612fb8ae424c27376e3ca4d0b4d9
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-03 20:53:11 -0300

    feat: create a logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Logo } from "./components/Logo";
+import { Container } from "./components/Container";
 
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+    </>
+  );
 }
