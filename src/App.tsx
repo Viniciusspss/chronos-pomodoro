46fd37f6f854805b03308a665dd1432a871ba712
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-03 20:08:00 -0300

    feat: create a container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import './styles/theme.css'
-import './styles/global.css'
-import { Heading } from './components/Heading'
-import { Timer } from 'lucide-react'
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
-
-  return (
-    <Heading>Hello World 
-      <button>
-        <Timer/>
-        </button>
-    </Heading>
-  )
+  return <></>;
 }
-
