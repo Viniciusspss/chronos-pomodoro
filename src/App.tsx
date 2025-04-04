1546eb301d6ccb0d5ee2dbb9952185697fcb8757
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-03 22:04:26 -0300

    feat: create a menu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { Logo } from "./components/Logo";
 import { Container } from "./components/Container";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+      <Container>
+        <Menu />
       </Container>
     </>
   );
