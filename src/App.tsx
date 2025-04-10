3f9e24038712c08f9599aedc75fd09b53a2f961f
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-10 19:32:59 -0300

    feat: create pages folder and add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Logo } from "./components/Logo";
-import { Container } from "./components/Container";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { Form } from "./components/Form";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <Form />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "../../components/Container";
+import { CountDown } from "../../components/CountDown";
+import { Form } from "../../components/Form";
+import { MainTemplate } from "./../../templates/MainTemplate/index";
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <Form />
+      </Container>
+    </MainTemplate>
+  );
+}
