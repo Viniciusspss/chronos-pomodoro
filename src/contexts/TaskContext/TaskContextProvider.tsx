891db8be78f799ba61c2164e0bd9a014c168d679
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-12 18:52:28 -0300

    refactor: refactor: move logic and components to dedicated folders

---
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { initialTaskState } from "./initialTaskState";
+import { TaskContext } from "./TaskContext";
+
+type TaskContextProvidesProps = {
+  children: React.ReactNode;
+};
+
+export function TaskContextProvider({ children }: TaskContextProvidesProps) {
+  const [state, setState] = useState(initialTaskState);
+  return (
+    <TaskContext.Provider value={{ state, setState }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
