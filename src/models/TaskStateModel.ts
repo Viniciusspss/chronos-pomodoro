a3890b2832eb9db5e559330be388fa3048026215
Vinicius Sátiro <viniciussatiro2@gmail.com>
2025-04-12 18:36:16 -0300

    feat: create Task context using useContext

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -4,7 +4,7 @@ export type TaskStateModel = {
   tasks: TaskModel[];
   secondsRemaining: number;
   formattedSecondsRemaining: string;
-  activeTaks: TaskModel | null;
+  activeTask: TaskModel | null;
   currentCycle: number;
   config: {
     workTime: number;
